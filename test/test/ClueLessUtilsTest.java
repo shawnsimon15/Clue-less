a8f1219638d0bafbb4d6fa54c589f7906dc09423
Melissa Huang <mhuang@Melissas-MacBook-Pro.local>
2020-10-24 16:11:02 -0700

    Added AutoMessageCheckTest.java -M.Huang

--- test/test/ClueLessUtilsTest.java
+++ test/test/ClueLessUtilsTest.java
@@ -1,40 +1,52 @@
 package test;
 
 import java.util.UUID;
-
 import org.json.simple.JSONObject;
-
 import main.java.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClueLessUtilsTest {
+	private UUID gameUUID;
 	
 	@org.junit.jupiter.api.Test
-	private void makePostTest()
+	public void makePostTest()
 	{
-		UUID gameUUID = UUID.randomUUID();
+		gameUUID = UUID.randomUUID();
 		ClueLessUtils.makePost(gameUUID, "Miss Scarlet", 3);
-		
-		JSONObject getJS = new JSONObject();
-		getJS.put("gameID", gameUUID);
-		getJS.put("playerName", "Miss Scarlet");
-		getJS.put("selectedPlayer", "Miss Scarlett");
-		
-		ClueLessUtils.makeGet(getJS.toString(), "");
-		
 	}
 	@org.junit.jupiter.api.Test
-	private void makePutTest()
+	public void makePutTest()
 	{
+		JSONObject movePlayer = new JSONObject();
+		movePlayer.put("gameID", gameUUID);
+		movePlayer.put("playerName", "Miss Scarlet");
+		movePlayer.put("newLocation", "Ballroom");
 		
+		ClueLessUtils.makePut(movePlayer.toString(), "");
 	}
 	@org.junit.jupiter.api.Test
-	private void makeGetTest()
+	public void makeGetTest()
 	{
+		JSONObject joinGame = new JSONObject();
+		joinGame.put("gameID", gameUUID);
+		joinGame.put("playerName", "Mr. Green");
 		
+		ClueLessUtils.makeGet(joinGame.toString(), "");
 	}
 	@org.junit.jupiter.api.Test
-	private void parseMessagesTest() 
+	public void parseMessagesTest() 
 	{
+		JSONObject makeSuggestion = new JSONObject();
+		makeSuggestion.put("gameID", gameUUID);
+		makeSuggestion.put("playerWhoSuggested", "Mr. Green");
+		JSONObject suggestions = new JSONObject();
+		suggestions.put("suspect", "Miss Scarlet");
+		suggestions.put("weapon", "Knife");
+		suggestions.put("location", "Kitchen");
+		
+		makeSuggestion.put("cardsSuggested", suggestions);
+		
+		ClueLessUtils.parseMessages(makeSuggestion.toString());
 		
 	}
 }
